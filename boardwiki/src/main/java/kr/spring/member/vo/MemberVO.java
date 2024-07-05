a133f991f541dc6003965088a1d9997cd681e138
kangsinhee1 <ssiniyaa@gmail.com>
2024-07-05 17:29:47 +0900

    memberVO 생성

--- boardwiki/src/main/java/kr/spring/member/vo/MemberVO.java
+++ boardwiki/src/main/java/kr/spring/member/vo/MemberVO.java
@@ -1,5 +1,7 @@
 package kr.spring.member.vo;
 
+import java.sql.Date;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -8,5 +10,16 @@ import lombok.ToString;
 @Setter
 @ToString
 public class MemberVO {
-	private long mem_num;
+	private long mem_num;//회원번호
+	private String mem_email;//회원 이메일(아이디)
+	private String mem_auth;//회원 등급
+	
+	private String mem_name;
+	private String mem_nickName;
+	private String mem_passwd;
+	private String mem_phone;
+	private String mem_provider;
+	private Date mem_rdate;
+	private Date mem_mdate;
+	private String mem_photo;
 }
