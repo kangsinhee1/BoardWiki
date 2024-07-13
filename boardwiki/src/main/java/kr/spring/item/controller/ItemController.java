31815f8db46c82130c1a7b73b7712b5e9f03a6ec
Pelpine <yoonsungbeab@gmail.com>
2024-07-13 15:52:44 +0900

    itemcontroller 빠진거 수정

--- boardwiki/src/main/java/kr/spring/item/controller/ItemController.java
+++ boardwiki/src/main/java/kr/spring/item/controller/ItemController.java
@@ -20,9 +20,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import kr.spring.cart.service.CartService;
 import kr.spring.cart.vo.CartVO;
 import kr.spring.item.service.ItemService;
 import kr.spring.item.vo.ItemVO;
+import kr.spring.member.vo.MemberVO;
 import kr.spring.util.PagingUtil;
 import lombok.extern.slf4j.Slf4j;
 
@@ -31,6 +33,8 @@ import lombok.extern.slf4j.Slf4j;
 public class ItemController {
 	@Autowired
 	public ItemService itemService;
+	@Autowired
+	private CartService cartService;
 
 	//자바빈(VO) 초기화
 	@ModelAttribute
@@ -128,7 +132,8 @@ public class ItemController {
 		model.addAttribute("maxtime",maxtime);
 
 		return new ModelAndView("item_detail","item",item); 
-	}/*=========================
+	}
+	/*=========================
 	 * 장바구니에 데이터 담기
 	 *=========================*/
 	@PostMapping("/item/item_detail")
@@ -143,9 +148,11 @@ public class ItemController {
 			return "/item/item_detail";
 		}
 
-		CartVO vo = (CartVO) session.getAttribute("cart");
-		cartVO.setItem_num(vo.getItem_num());
-
+		MemberVO vo = (MemberVO)session.getAttribute("user");
+		cartVO.setMem_num(vo.getMem_num());
+		
+		cartService.insertCart(cartVO);
+		
 		model.addAttribute("message", "상품이 장바구니에 추가되었습니다.");
 		model.addAttribute("url", request.getContextPath() + "/item/item_detail");
 
